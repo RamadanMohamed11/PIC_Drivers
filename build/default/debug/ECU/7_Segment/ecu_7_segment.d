cb68ecb214d787a71eeebe6483f133061c79cddc
Ramadan Mohamed <elking.medo61111@gmail.com>
2025-11-02 01:14:11 +0200

    Refactor interrupt configuration and update related files
    - Added a feature toggle for interrupt priority in mcal_interrupt_config.h.
    - Adjusted the definitions for enabling and disabling global and peripheral interrupts based on the new feature toggle.

---
+++ build/default/debug/ECU/7_Segment/ecu_7_segment.d
@@ -0,0 +1,7 @@
+build/default/debug/ECU/7_Segment\ecu_7_segment.p1: \
+  ECU/7_Segment/ecu_7_segment.c ECU/7_Segment/ecu_7_segment.h \
+  ECU/7_Segment/../../MCAL/GPIO/hal_gpio.h \
+  ECU/7_Segment/../../MCAL/GPIO/../std_types.h \
+  ECU/7_Segment/../../MCAL/GPIO/../compiler.h \
+  ECU/7_Segment/../../MCAL/GPIO/../std_libraries.h \
+  ECU/7_Segment/../../MCAL/GPIO/../device_config.h
