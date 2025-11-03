4a381fdf83b493f03b234611229e921f8880e0ae
Ramadan Mohamed <elking.medo61111@gmail.com>
2025-11-03 07:39:16 +0200

    Add clear flag and callback functions for external interrupts

--- MCAL/Interrupt/mcal_external_interrupt.c
+++ MCAL/Interrupt/mcal_external_interrupt.c
@@ -12,10 +12,20 @@ static Std_ReturnType EXT_INTx_PIN_Init(const ext_INTx_config_t* ext_INTx);
 static Std_ReturnType EXT_INTx_Enable(const ext_INTx_config_t* ext_INTx);
 static Std_ReturnType EXT_INTx_Disable(const ext_INTx_config_t* ext_INTx);
 static Std_ReturnType EXT_INTx_edge_Init(const ext_INTx_config_t* ext_INTx);
-
+static Std_ReturnType EXT_INTx_Clear_Flag(const ext_INTx_config_t* ext_INTx);
+static Std_ReturnType EXT_INTx_Set_Callback(const ext_INTx_config_t* ext_INTx);
 #if INTERRUPT_PRIORITY_FEATURE_ENABLE == INTERRUPT_PRIORITY_ENABLE
 static Std_ReturnType EXT_INTx_priority_Init(const ext_INTx_config_t* ext_INTx);
 #endif /* INTERRUPT_PRIORITY_FEATURE_ENABLE == INTERRUPT_PRIORITY_ENABLE */
+
+static Std_ReturnType EXT_RBx_PIN_Init(const ext_RBx_config_t* ext_INTx);
+static Std_ReturnType EXT_RBx_Enable(const ext_RBx_config_t* ext_INTx);
+static Std_ReturnType EXT_RBx_Disable(const ext_RBx_config_t* ext_INTx);
+static Std_ReturnType EXT_RBx_edge_Init(const ext_RBx_config_t* ext_INTx);
+#if INTERRUPT_PRIORITY_FEATURE_ENABLE == INTERRUPT_PRIORITY_ENABLE
+static Std_ReturnType EXT_RBx_priority_Init(const ext_RBx_config_t* ext_INTx);
+#endif /* INTERRUPT_PRIORITY_FEATURE_ENABLE == INTERRUPT_PRIORITY_ENABLE */
+
 
 Std_ReturnType EXT_INTx_Init(const ext_INTx_config_t* ext_INTx)
 {
@@ -24,7 +34,20 @@ Std_ReturnType EXT_INTx_Init(const ext_INTx_config_t* ext_INTx)
         state=E_NOT_OK;
     else
     {
-
+        // Disable interrupt during configuration
+        state &= EXT_INTx_Disable(ext_INTx);
+        // Clear interrupt flag
+        state &= EXT_INTx_Clear_Flag(ext_INTx);
+        // Initialize pin direction
+        state &= EXT_INTx_PIN_Init(ext_INTx);
+        // Initialize interrupt edge
+        state &= EXT_INTx_edge_Init(ext_INTx);
+        // Initialize interrupt priority
+        state &= EXT_INTx_priority_Init(ext_INTx);
+        // Interrupt Callback function assignment would go here
+        state &= EXT_INTx_Set_Callback(ext_INTx);
+        // Enable interrupt
+        state &= EXT_INTx_Enable(ext_INTx);
     }
     return state;
 }
@@ -48,7 +71,7 @@ static Std_ReturnType EXT_INTx_PIN_Init(const ext_INTx_config_t* ext_INTx)
         state=E_NOT_OK;
     else
     {
-
+        state &= gpio_pin_direction_initialize(&ext_INTx->pin_config);
     }
     return state;
 }
@@ -148,6 +171,44 @@ static Std_ReturnType EXT_INTx_edge_Init(const ext_INTx_config_t* ext_INTx)
                 state=E_NOT_OK;
                 break;
         }
+    }
+    return state;
+}
+
+static Std_ReturnType EXT_INTx_Clear_Flag(const ext_INTx_config_t* ext_INTx)
+{
+    Std_ReturnType state=E_OK;
+    if(ext_INTx==NULL)
+        state=E_NOT_OK;
+    else
+    {
+        switch(ext_INTx->int_source)
+        {
+            case EXT_INT0:
+                EXT_INT0_INTERRUPT_FLAG_CLEAR();
+                break;
+            case EXT_INT1:
+                EXT_INT1_INTERRUPT_FLAG_CLEAR();
+                break;
+            case EXT_INT2:
+                EXT_INT2_INTERRUPT_FLAG_CLEAR();
+                break;
+            default:
+                state=E_NOT_OK;
+                break;
+        }
+    }
+    return state;
+}
+
+static Std_ReturnType EXT_INTx_Set_Callback(const ext_INTx_config_t* ext_INTx)
+{
+    Std_ReturnType state=E_OK;
+    if(ext_INTx==NULL)
+        state=E_NOT_OK;
+    else
+    {
+
     }
     return state;
 }
